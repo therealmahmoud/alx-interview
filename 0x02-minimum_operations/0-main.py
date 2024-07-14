4a657ef28d0ebbf5d2ae6f5a06932d5ea0302a03
therealmahmoud <moha91875460@gmail.com>
2024-07-14 07:17:51 +0300

    Updated

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -5,7 +5,7 @@ Main file for testing
 
 minOperations = __import__('0-minoperations').minOperations
 
-n = 5
+n = 20
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,22 +6,18 @@ def minOperations(n: int) -> int:
     """ Calculates the fewest number of operations needed
     to result in exactly n H characters in the file."""
     count: int = 0
-    copy: int = 0
-    char: int = 1
-    while char < n:
-        if copy == 0:
+    copy: str = ""
+    char: str = "H"
+    while len(char) < n:
+        if len(copy) == 0:
             copy = char
             char += copy
             count += 2
-        elif char + copy == n:
-            char + copy
-            count += 1
-            break
-        elif char % 2 != 0:
+        elif n - len(char) > 0 and (n - len(char)) % len(char) == 0:
             copy = char
             char += copy
             count += 2
-        elif copy > 0:
+        elif len(copy) > 0:
             char += copy
             count += 1
     return count
