1a28e00ced28396886976c4f4523ff82ae8ceac3
therealmahmoud <moha91875460@gmail.com>
2024-07-14 04:55:59 +0300

    Updated

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,24 +2,26 @@
 """ An alx interview preparation question. """
 
 
-def minOperations(n) -> int:
+def minOperations(n: int) -> int:
     """ Calculates the fewest number of operations needed
     to result in exactly n H characters in the file."""
-    if not isinstance(n, int):
-        return 0
     count: int = 0
     copy: str = ""
     char: str = "H"
     while len(char) < n:
-        if len(char) + len(copy) == n:
+        if len(copy) == 0:
+            copy = char
+            char += copy
+            count += 2
+        elif len(char) + len(copy) == n:
             char + copy
             count += 1
             break
-        elif len(char) % 2 != 0:
+        elif len(char) % 2 != 0: # for Odds
             count += 2
             copy = char
             char += copy
-        elif len(char) % 2 == 0:
+        elif len(char) % 2 == 0: # for Even
             char += copy
             count += 1
     return count
